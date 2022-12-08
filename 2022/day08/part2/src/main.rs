841d685d0aa134b83b931a397d2b93b8ec9700fc
Fritz Rehde <fritzrehde.work@gmail.com>
2022-12-08 19:48:35 +0100

    Cleaned up day 8 solution

--- 2022/day08/part2/src/main.rs
+++ 2022/day08/part2/src/main.rs
@@ -15,53 +15,6 @@ fn main() -> io::Result<()> {
 		})
 		.collect();
 
-	// let max_scenic_score = trees
-	// 	.clone()
-	// 	.iter()
-	// 	.enumerate()
-	// 	.map(|(y, tree_line)| {
-	// 		tree_line.iter().enumerate().map(|(x, tree)| {
-	// 			let mut scores = [0, 0, 0, 0];
-
-	// 			// right score
-	// 			for x_move in x + 1..trees[0].len() {
-	// 				scores[0] += 1;
-	// 				if trees[y][x_move] > *tree {
-	// 					break;
-	// 				}
-	// 			}
-
-	// 			// left score
-	// 			for x_move in (0..x).rev() {
-	// 				scores[1] += 1;
-	// 				if trees[y][x_move] > *tree {
-	// 					break;
-	// 				}
-	// 			}
-
-	// 			// down score
-	// 			for y_move in y + 1..trees.len() {
-	// 				scores[2] += 1;
-	// 				if trees[y_move][x] > *tree {
-	// 					break;
-	// 				}
-	// 			}
-
-	// 			// up score
-	// 			for y_move in (0..y).rev() {
-	// 				scores[3] += 1;
-	// 				if trees[y_move][x] > *tree {
-	// 					break;
-	// 				}
-	// 			}
-
-	// 			scores.iter().product::<u32>()
-	// 		})
-	// 	})
-	// 	.flatten()
-	// 	.max()
-	// 	.unwrap();
-
 	let mut max_scenic_score = 0;
 
 	for (y, tree_line) in trees.iter().enumerate() {
@@ -69,7 +22,7 @@ fn main() -> io::Result<()> {
 			let mut scores = [0, 0, 0, 0];
 
 			// right score
-			for x_move in x+1..trees[0].len() {
+			for x_move in x + 1..trees[0].len() {
 				scores[0] += 1;
 				if trees[y][x_move] >= *tree {
 					break;
@@ -85,7 +38,7 @@ fn main() -> io::Result<()> {
 			}
 
 			// down score
-			for y_move in y+1..trees.len() {
+			for y_move in y + 1..trees.len() {
 				scores[2] += 1;
 				if trees[y_move][x] >= *tree {
 					break;
@@ -101,11 +54,6 @@ fn main() -> io::Result<()> {
 			}
 
 			max_scenic_score = std::cmp::max(scores.iter().product::<u32>(), max_scenic_score);
-			// let scenic_score: u32 = scores.iter().product();
-			// if scenic_score > max_scenic_score {
-			// 	max_scenic_score = scenic_score;
-			// }
-			
 		}
 	}
 
