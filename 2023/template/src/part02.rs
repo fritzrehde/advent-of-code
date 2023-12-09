44d792b4154e947f52bbec32503c90fae27ee4bb
fritzrehde <fritzrehde.work@gmail.com>
2023-12-09 14:38:49 +0100

    Added tests for the part01 and part02 examples to template

--- 2023/template/src/part02.rs
+++ 2023/template/src/part02.rs
@@ -1,3 +1,18 @@
+#[cfg(test)]
+pub mod example {
+    use indoc::indoc;
+
+    /// Provide the example details as `(puzzle input, expected solution)`.
+    pub fn example_details() -> (&'static str, String) {
+        let puzzle_input = indoc! {"
+            puzzle input lines
+        "};
+        let expected_solution = 42;
+        (puzzle_input, expected_solution.to_string())
+    }
+}
+
+/// Solve the problem and return the solution as a `String`.
 pub fn solve(puzzle_input: &str) -> anyhow::Result<String> {
     Ok("todo".to_string())
 }
