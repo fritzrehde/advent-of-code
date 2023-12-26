2d72b1df10aef5abd146d86ad8d5e441b277ccfa
fritzrehde <fritzrehde.work@gmail.com>
2023-12-26 18:07:02 +0100

    Solved day19 part 1

---
+++ 2023/day19/src/part02.rs
@@ -0,0 +1,19 @@
+/// Solve the problem and return the solution as a `String`.
+pub fn solve(puzzle_input: &str) -> anyhow::Result<String> {
+    Ok("todo".to_string())
+}
+
+#[cfg(test)]
+pub mod example {
+    use indoc::indoc;
+
+    /// Provide multiple example details as `[(puzzle input, expected solution)]`.
+    pub fn example_details() -> impl Iterator<Item = (&'static str, String)> {
+        let puzzle_input = indoc! {"
+            puzzle input lines
+        "};
+        let expected_solution = 42;
+
+        [(puzzle_input, expected_solution.to_string())].into_iter()
+    }
+}
