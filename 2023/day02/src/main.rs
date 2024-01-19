252ef537277386e9f4b9d47c9f77d22a3f656935
fritzrehde <fritzrehde.work@gmail.com>
2024-01-20 00:28:22 +0100

    build: use cargo workspace to share common dependencies

--- 2023/day02/src/main.rs
+++ 2023/day02/src/main.rs
@@ -1,10 +1,8 @@
 mod part01;
 mod part02;
 
-use std::fs::read_to_string;
-
 fn main() -> anyhow::Result<()> {
-    let puzzle_input = read_to_string("puzzle_input.txt")?;
+    let puzzle_input = include_str!("../puzzle_input.txt");
 
     println!("Part 01: {}", part01::solve(&puzzle_input)?);
     println!("Part 02: {}", part02::solve(&puzzle_input)?);
