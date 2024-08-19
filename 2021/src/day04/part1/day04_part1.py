badbf66fb92c881078b9975aa346034647f803e3
fritzrehde <fritzrehde.work@gmail.com>
2024-08-19 14:31:11 +1000

    2021(day04 part1): solved

---
+++ 2021/src/day04/part1/day04_part1.py
@@ -0,0 +1,81 @@
+from dataclasses import dataclass
+from typing import List
+from puzzle_utils import puzzle, run
+
+
+@dataclass
+class Cell:
+    num: int
+    marked: bool
+
+    def __init__(self, num: int):
+        self.num = num
+        self.marked = False
+
+    def mark(self):
+        self.marked = True
+
+
+@dataclass
+class Board:
+    rows: List[List[Cell]]
+
+    def __init__(self, lines: List[str]):
+        self.rows = [[Cell(int(num)) for num in row.split()] for row in lines]
+
+    def cells_in_row(self, row_idx: int):
+        return self.rows[row_idx]
+
+    def cells_in_col(self, col_idx: int):
+        # TODO: very inefficient, we're duplicating the whole matrix every time.
+        return list(zip(*self.rows))[col_idx]
+
+    def get_unmarked_cells(self):
+        for row in self.rows:
+            for cell in row:
+                if not cell.marked:
+                    yield cell
+
+    def wins_after_mark(self, marked_num: int) -> bool:
+        """
+        Mark all cells on the board that have the given value (there may be
+        multiple), and return whether this board has won.
+        """
+
+        def latest_marked_coords():
+            for row_idx, row in enumerate(self.rows):
+                for col_idx, cell in enumerate(row):
+                    if cell.num == marked_num:
+                        cell.mark()
+                        yield (row_idx, col_idx)
+
+        for row_idx, col_idx in latest_marked_coords():
+            if all(cell.marked for cell in self.cells_in_row(row_idx)):
+                return True
+            if all(cell.marked for cell in self.cells_in_col(col_idx)):
+                return True
+
+        return False
+
+
+@puzzle
+def solve(puzzle_input: str) -> str:
+    sections = puzzle_input.split("\n\n")
+
+    numbers = list(map(int, sections[0].split(",")))
+    boards = [Board(board.splitlines()) for board in sections[1:]]
+
+    for number in numbers:
+        for board in boards:
+            if board.wins_after_mark(number):
+                unmarked_sum = sum(
+                    map(lambda cell: cell.num, board.get_unmarked_cells())
+                )
+                result = unmarked_sum * number
+                return str(result)
+
+    raise Exception("unreachable: assume one of the boards wins")
+
+
+if __name__ == "__main__":
+    run(solve)
