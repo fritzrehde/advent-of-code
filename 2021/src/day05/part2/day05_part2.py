2fccde9e60a3d6260652f5e21e4f818f821cee5e
fritzrehde <fritzrehde.work@gmail.com>
2024-08-19 16:50:26 +1000

    2021(day05 part2): solved

--- 2021/src/day05/part2/day05_part2.py
+++ 2021/src/day05/part2/day05_part2.py
@@ -23,16 +23,15 @@ def solve(puzzle_input: str) -> str:
                 print(p if (p := grid[y][x]) != 0 else ".", end="")
             print()
 
-    # TODO: Technically, this impl of bi_range is incorrect (if the
-    # order/direction of range should be maintained, e.g. bi_range(8,6) should
-    # be 8,7,6 but this fn returns 6,7,8), but kept here so it can be compared
-    # to the correct implementation in part 2.
     def bi_range(from_incl: int, to_incl: int):
-        """Bi-directional range."""
+        """
+        Generates a range between endpoints, figuring in which direction
+        (towards infinity or negative infinity) to iterate.
+        """
         if from_incl <= to_incl:
             return range(from_incl, to_incl + 1)
         else:
-            return range(to_incl, from_incl + 1)
+            return range(from_incl, to_incl - 1, -1)
 
     def horiztonal_or_vertical_line(x1, y1, x2, y2) -> bool:
         return x1 == x2 or y1 == y2
@@ -44,6 +43,9 @@ def solve(puzzle_input: str) -> str:
                 for y in bi_range(y1, y2):
                     for x in bi_range(x1, x2):
                         grid[y][x] += 1
+            else:
+                for x, y in zip(bi_range(x1, x2), bi_range(y1, y2)):
+                    grid[y][x] += 1
 
     result = ilen(filter(lambda point: point >= 2, all_points()))
 
